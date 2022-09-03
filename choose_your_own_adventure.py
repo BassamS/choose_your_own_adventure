f3fc9568af935f35bf34c95c2e80a2b6dd8bb85f
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-03 07:11:42 +0200

    2nd commit

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -1,12 +1,11 @@
 name = input("Type your name:  ")
 print(f"welcome {name} to this adventure!")
 
-answer = print("You are on a dirt road, it has come to an end and left or right. Which way would you like to go? (left or right?)").lower()
+answer = input("You are on a dirt road, it has come to an end and left or right. Which way would you like to go? (left or right?): ").lower()
 
-if answer == "left":
+if answer == "left":  
   
-  
-  answer = input("You come to a river, you can swim across or walk around it? type (swim) to swim across or(walk) to walk around: ")
+  answer = input("You come to a river, you can swim across or walk around it? type (swim) to swim across or(walk) to walk around: ").lower()
 
   if answer == "swim":
     print("You swam across the river and were eaten by an allegator. You lost the game!")
@@ -16,6 +15,6 @@ if answer == "left":
     print("Not a valid option. You loose!")
 
 elif answer == "right":
-
+  answer = input("You come a bridge, it looks wobbly, do you want to cross it or head back (cross/back)? ").lower()
 else:
-  print("Not a valid option. You loose!")  
+  print("Not a valid option. You loose!")
